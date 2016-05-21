d6dd8d6fd6215e780ef5796d0c7a024575c287b1
emjayce3 <morgan.cave@gmail.com>
2016-05-21 10:30:42 +1000

    first upload
    glhf

---
+++ js/custom.js
@@ -0,0 +1,91 @@
+$(function(){
+	//jQuery methods go here...
+  $(document).ready(function(){
+  	$(".sns1").hide();
+  	$(".sns2").hide();
+	$(".sns3").hide();
+	$(".sn1-1").hide();
+	$(".sn2-1").hide();
+	$(".sn3-1").hide();
+  });
+
+
+  $(".sn1").click(function(){
+        $(".sns1").slideToggle();
+    });
+
+  $(".sn2").click(function(){
+        $(".sns2").slideToggle();
+    });
+
+  $(".sn3").click(function(){
+        $(".sns3").slideToggle();
+    });
+
+  $(".sn1-1").click(function(){
+        $(".sns1").slideToggle();
+    });
+
+  $(".sn2-1").click(function(){
+        $(".sns2").slideToggle();
+    });
+
+  $(".sn3-1").click(function(){
+        $(".sns3").slideToggle();
+    });
+
+  $(".sn1").css('cursor', 'pointer');
+
+  $(".sn2").css('cursor', 'pointer');
+
+  $(".sn3").css('cursor', 'pointer');
+
+  $(".sn1-1").css('cursor', 'pointer');
+
+  $(".sn2-1").css('cursor', 'pointer');
+
+  $(".sn3-1").css('cursor', 'pointer');
+
+  $(".sn1-1").click(function(){
+    $(".sn1").show();
+  });
+  
+  $(".sn1-1").click(function(){
+    $(".sn1-1").hide();
+  });
+
+  $(".sn1").click(function(){
+    $(".sn1-1").show();
+  });
+  
+  $(".sn1").click(function(){
+    $(".sn1").hide();
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
